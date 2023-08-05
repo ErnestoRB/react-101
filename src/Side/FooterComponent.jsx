a426ccbdf4e8dd8f539ce0d21b4acd8314d469ef
Ernesto Ramírez <ernestorb_@outlook.com>
2023-08-04 22:44:38 -0600

    Mejorar acomodo de los componentes

--- src/Side/FooterComponent.jsx
+++ src/Side/FooterComponent.jsx
@@ -2,28 +2,49 @@ import "./Footer.css";
 
 export default function FooterComponent() {
   return (
-    <div className="Content">
-      <a href="" className="links">Información</a>
+    <footer className="Footer-Content">
+      <a href="" className="links">
+        Información
+      </a>
       <p>·</p>
-      <a href="" className="links">Ayuda</a>
+      <a href="" className="links">
+        Ayuda
+      </a>
       <p>·</p>
-      <a href="" className="links">Prensa</a>
+      <a href="" className="links">
+        Prensa
+      </a>
       <p>·</p>
-      <a href="" className="links">API</a>
+      <a href="" className="links">
+        API
+      </a>
       <p>·</p>
-      <a href="" className="links">Empleo</a>
+      <a href="" className="links">
+        Empleo
+      </a>
       <p>·</p>
-      <a href="" className="links">Privacidad</a>
+      <a href="" className="links">
+        Privacidad
+      </a>
       <p>·</p>
-      <a href="" className="links">Condiciones</a>
+      <a href="" className="links">
+        Condiciones
+      </a>
       <p>·</p>
-      <a href="" className="links">Ubicaciones</a>
+      <a href="" className="links">
+        Ubicaciones
+      </a>
       <p>·</p>
-      <a href="" className="links">Idioma</a>
+      <a href="" className="links">
+        Idioma
+      </a>
       <p>·</p>
-      <a href="" className="links">Meta Verified</a>
-      <br /><br />
+      <a href="" className="links">
+        Meta Verified
+      </a>
+      <br />
+      <br />
       <p>© 2023 INSTAGRAM FROM META</p>
-    </div>
+    </footer>
   );
 }
--- src/Stories/StoriesComponent.jsx
+++ src/Stories/StoriesComponent.jsx
@@ -2,10 +2,12 @@ import StoryComponent from "./StoryComponent";
 import "./Stories.css";
 export default function StoriesComponent({ stories = [] }) {
   return (
-    <div className="Stories">
-      {stories.map((story) => (
-        <StoryComponent isClose={false}></StoryComponent>
-      ))}
+    <div className="Stories-Wrapper">
+      <div className="Stories">
+        {stories.map((story) => (
+          <StoryComponent isClose={false}></StoryComponent>
+        ))}
+      </div>
     </div>
   );
 }
