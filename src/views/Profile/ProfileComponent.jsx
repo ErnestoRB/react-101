39a86c2267e7b184a826cd753d9d79239f7b8543
Haitore <ender12as@gmail.com>
2023-08-13 17:49:03 -0600

    Encabezado de perfil

--- src/views/Profile/ProfileComponent.jsx
+++ src/views/Profile/ProfileComponent.jsx
@@ -1,3 +1,46 @@
+import StoryComponent from "../../Stories/StoryComponent";
+import UserImageComponent from "../../User/UserImageComponent";
+import useMe from "../../hooks/useMe";
+import "./Profile.css";
+
 export default function ProfileComponent() {
-  return <div>Profile</div>;
+  const { me } = useMe();
+
+  return (
+    <div className="MainProfile">
+      {me && (
+        <>
+          <div className="display">
+            <div className="firstSec">
+              <UserImageComponent
+                src={me.author.picture.large}
+                size={150}
+              ></UserImageComponent>
+            </div>
+            <div className="secondSec">
+              <div className="nameOptions">
+                <p>{me.author.username}</p>
+                <div className="buttons">
+                  <button className="editProfile">Editar perfil</button>
+                  <button className="editProfile">Ver archivo</button>
+                </div>
+                <a href=""><span class="material-symbols-outlined">settings</span></a>
+              </div>
+              <div className="userData">
+                <p>publicaciones</p>
+                <p>seguidores</p>
+                <p>seguidos</p>
+              </div>
+              <div className="userBio">
+                <h4>{me.author.name}</h4>
+                <p>Bio example.</p>
+              </div>
+            </div>
+          </div>
+          <div className="highlights">
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
